a3170b55304829e123f65378babad3580e92623b
Jozef Mlich <jmlich83@gmail.com>
2021-09-03 14:16:22 +0000

    add i18n support

---
+++ translations/glacier-calc_cs.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs_CZ">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/main.cpp" line="50"/>
+        <source>Calculator</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>glacier-calc</name>
+    <message>
+        <location filename="../src/qml/glacier-calc.qml" line="68"/>
+        <source>Calculator</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
